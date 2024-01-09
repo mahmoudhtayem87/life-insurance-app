bd812e1212dc1178184eb92391f9b00ac1c48692
Mahmoud Hussein Tayem <tayemmahmoud87@gmail.com>
2024-01-09 20:33:53 +0400

    first push

---
+++ screens/login/login.screen.js
@@ -0,0 +1,63 @@
+import {useState} from "react";
+import {Button, Dialog, Icon, Input, Overlay} from "@rneui/base";
+import {ActivityIndicator, View} from "react-native";
+import LoginStyle from "./login.screen.style";
+import banner from '../../assets/images/login/banner.png';
+import { Image } from '@rneui/themed';
+import {Login} from "../../services/auth.service";
+
+const LoginScreen = ({ navigation }) => {
+    const [username, setUsername] = useState('admin@lifeinsurances.com');
+    const [password, setPassword] = useState('test123');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleLogin = async () => {
+        setIsLoading(true);
+        let result = await Login(username,password);
+        if (result) {
+            navigation.navigate('Home',{username:username,password:password});
+        }else {
+            alert('Invalid user name / password!');
+        }
+        setIsLoading(false);
+    };
+
+    return (
+        <View style={LoginStyle.mainContainer}>
+                    <Dialog  isVisible={isLoading}>
+                        <Dialog.Loading />
+                    </Dialog>
+            <View style={LoginStyle.bannerContainer}>
+                <Image
+                    style={LoginStyle.banner}
+                    source={banner}
+                    PlaceholderContent={<ActivityIndicator />}
+                />
+            </View>
+            <View style={LoginStyle.view}>
+                <Input
+                    style={LoginStyle.input}
+                    placeholder="Username"
+                    value={username}
+                    onChangeText={setUsername}
+                    leftIcon={<Icon name="user" type={'font-awesome'}  color={LoginStyle.icon.color} size={20}/>}
+                />
+                <Input
+                    style={LoginStyle.input}
+                    placeholder="Password"
+                    secureTextEntry
+                    value={password}
+                    onChangeText={setPassword}
+                    leftIcon={<Icon name="lock" color={LoginStyle.icon.color} size={20}/>}
+                />
+                <View style={LoginStyle.actions}>
+                    <Button style={LoginStyle.button} color={LoginStyle.button.backgroundColor} title="Login" disabled={username.length <= 0 || password.length <= 0 } onPress={handleLogin} />
+                    <Button style={LoginStyle.button} color={LoginStyle.button.backgroundColor} title="Register" />
+                </View>
+            </View>
+        </View>
+
+    );
+};
+
+export default LoginScreen;
---
+++ screens/login/login.screen.style.js
@@ -0,0 +1,77 @@
+import {StyleSheet} from 'react-native';
+import {Dimensions} from 'react-native';
+import ThemeVariables from "../../theme/theme.variables";
+import themeVariables from "../../theme/theme.variables";
+
+
+const LoginStyle = StyleSheet.create({
+    loadingContainer:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden',
+    },
+    mainContainer:{
+        flex: 1,
+        overflow: 'hidden',
+        backgroundColor: ThemeVariables.color.primary
+    },
+    view: {
+        flex: 1,
+        justifyContent: 'top',
+        alignItems: 'center',
+        overflow: 'hidden',
+        borderTopRightRadius:themeVariables.border.radius.large,
+        borderBottomRightRadius:themeVariables.border.radius.large,
+        borderTopLeftRadius:themeVariables.border.radius.large,
+        borderBottomLeftRadius:themeVariables.border.radius.large,
+        top:-50,
+        padding:themeVariables.padding.big,
+        margin:themeVariables.margin.small,
+        backgroundColor: ThemeVariables.color.dark
+    },
+    bannerContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width:'100%',
+        height:Dimensions.get('window').height / 2,
+        overflow: 'hidden',
+        backgroundColor: ThemeVariables.color.dark
+    },
+    scrollView: {
+        flex: 1,
+        backgroundColor: ThemeVariables.color.lightGray
+    },
+    container: {
+        flex: 1,
+        width: Dimensions.get('window').width
+    },
+    input:{
+        color:themeVariables.color.white,
+    },
+    icon:{
+        color: themeVariables.color.primary
+    },
+    button:{
+        backgroundColor:themeVariables.color.primary,
+        minWidth:themeVariables.button.size.medium
+    },
+    banner: {
+        flex: 1, // Take up all available space within the container
+        resizeMode:"cover",
+        height:'100%',
+        width:Dimensions.get('window').width
+
+    },
+    actions:{
+        flex: 1,
+        display:"flex",
+        flexDirection: 'row', // Arrange children components horizontally
+        justifyContent: 'space-between', // Distribute space evenly between children components
+        alignItems: 'start',
+        width:'100%',
+        padding:themeVariables.padding.small,
+    }
+});
+export default LoginStyle;
